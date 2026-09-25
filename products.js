b4a14e9c909c5fd31864dfc54cd5a4416bad98bd
daitouguppy <e0982711665@yahoo.com.tw>
2026-09-26 04:49:59 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -1,6 +1,38 @@
-const PRODUCTS = [
- {id:1, name:"#1 自改良極血紅大背緞帶", subtitle:"粗體原線公｜實拍影片①", price:900, stock:5, desc:"1公、1緞母、1短母", video:"LINE_MOVIE_1790313978285_web.mp4"},
- {id:2, name:"#2 自改良極血紅大背緞帶", subtitle:"粗體原線公｜實拍影片②", price:900, stock:5, desc:"1公、1緞母、1短母", video:"LINE_MOVIE_1790313997010_web.mp4"},
- {id:3, name:"#3 自改良極血紅大背緞帶", subtitle:"粗體原線公｜實拍影片③", price:900, stock:5, desc:"1公、1緞母、1短母", video:"LINE_MOVIE_1790314006514_web.mp4"},
- {id:4, name:"#4 自改良極血紅大背緞帶", subtitle:"粗體原線公｜實拍影片④", price:900, stock:5, desc:"1公、1緞母、1短母", video:"LINE_MOVIE_1790314029057_web.mp4"}
-];
+const products = [
+  {
+    "id": 1,
+    "name": "#1 自改良極血紅大背緞帶",
+    "price": 1200,
+    "stock": 5,
+    "video": "LINE_MOVIE_1790313978285_web.mp4",
+    "subtitle": "粗體原線公｜實拍影片①",
+    "desc": "1公、1緞母、1短母"
+  },
+  {
+    "id": 2,
+    "name": "#2 自改良極血紅大背緞帶",
+    "price": 900,
+    "stock": 5,
+    "video": "LINE_MOVIE_1790313997010_web.mp4",
+    "subtitle": "粗體原線公｜實拍影片②",
+    "desc": "1公、1緞母、1短母"
+  },
+  {
+    "id": 3,
+    "name": "#3 自改良極血紅大背緞帶",
+    "price": 900,
+    "stock": 5,
+    "video": "LINE_MOVIE_1790314006514_web.mp4",
+    "subtitle": "粗體原線公｜實拍影片③",
+    "desc": "1公、1緞母、1短母"
+  },
+  {
+    "id": 4,
+    "name": "#4 自改良極血紅大背緞帶",
+    "price": 900,
+    "stock": 5,
+    "video": "LINE_MOVIE_1790314029057_web.mp4",
+    "subtitle": "粗體原線公｜實拍影片④",
+    "desc": "1公、1緞母、1短母"
+  }
+];\n
